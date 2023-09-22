f816bf128718eef6005ca5898206aaa602caa351
damnpav <hcanilvap@gmail.com>
2023-09-23 02:58:19 +0300

    finance api1

--- quote_portal/frontpage/quotes_data_update.py
+++ quote_portal/frontpage/quotes_data_update.py
@@ -1,13 +1,15 @@
 import yfinance as yf
+import pandas as pd
 
 
-def get_data(symbol):
+def get_data(symbols):
     """
     Function to get financial quotes from API
+    symbols - List with names of currencies (['EURUSD=X', 'GBPUSD=X'])
     :return:
     """
     quotes_data = yf.download(  # download Df with historical data
-        tickers=symbol,
+        tickers=symbols,
         period="max",
         interval="1m",
         group_by='ticker',
@@ -15,10 +17,15 @@ def get_data(symbol):
         prepost=True,
         threads=True,
         proxy=None)
-    quotes_data = quotes_data.reset_index()
-    quotes_data = quotes_data[['Datetime', 'Close']]
-    quotes_data['Datetime'] = quotes_data['Datetime'].astype(str)
-    return quotes_data
+
+    quotes_data_list = []
+    for symbol in symbols:
+        current_df = quotes_data[symbol].copy()
+        current_df = current_df.reset_index()
+        current_df = current_df[['Datetime', 'Close']]
+        current_df['Datetime'] = current_df['Datetime'].astype(str)
+        quotes_data_list.append(current_df)
+    return quotes_data_list
 
 # symbol = 'EURUSD=X'
 # quotes_data = get_data(symbol)
--- quote_portal/frontpage/views.py
+++ quote_portal/frontpage/views.py
@@ -3,13 +3,37 @@ from django.http import HttpResponse
 from django.template import loader
 import plotly.express as px
 import plotly.io as pio
-from quotes_data_update import get_data
+import plotly.graph_objects as go
+import plotly.io
+from . import quotes_data_update as qdu
 
 
 def frontpage_start(request):
-    quote_data = get_data('EURUSD=X')
-    fig = px.line(quote_data, x='Datetime', y='Close')
-    graph_html = pio.to_html(fig, full_html=False)
+    symbols = ['EURUSD=X', 'GBPUSD=X']
+    quotes_data_list = qdu.get_data(symbols)
+    trace_list = []
+
+    # Create a trace for each DataFrame in the list
+    for idx, df in enumerate(quotes_data_list):
+        trace = go.Scatter(x=df['Datetime'], y=df['Close'], name=f'{symbols[idx]}')
+        trace_list.append(trace)
+
+    # Define buttons for the dropdown menu, one for each DataFrame and one to show all
+    buttons = [dict(label='All', method='update', args=[{'visible': [True] * len(quotes_data_list)}])]
+    buttons += [
+        dict(label=f'{symbols[idx]}', method='update',
+             args=[{'visible': [idx == j for j in range(len(quotes_data_list))]}])
+        for idx in range(len(quotes_data_list))]
+
+    # Define layout with dropdown menu
+    layout = go.Layout(
+        title='Currencies Plot',
+        updatemenus=[dict(buttons=buttons)]
+    )
+
+    # Create Figure and convert to HTML
+    fig = go.Figure(data=trace_list, layout=layout)
+    graph_html = plotly.io.to_html(fig, full_html=False)
     return render(request, 'frontpage.html', {'graph': graph_html})
 
 
